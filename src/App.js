3683717035f9fcee89ec1d42afa696a2e67024ae
Sidney Correia <sidneycorreia@MacBook-Pro-de-Sidney.local>
2020-02-14 06:20:40 -0300

    Substituindo a forma de usar o tabler

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Layout from './components/Layout';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ForgotPasswordPage,
+  RegisterPage,
+  Error400,
+  Error401,
+  Error403,
+  Error404,
+  Error500,
+  Error503,
+  Empty,
+  Email,
+  ProfilePage,
+} from "./pages";
+
+import HomePage from "./HomePage.react";
+import FormElementsPage from "./FormElementsPage.react";
+import StoreCardsPage from "./components/StoreCardsPage.react.js";
+import BlogPage from "./components/BlogPage.react";
+
+import "tabler-react/dist/Tabler.css";
 
 class App extends React.Component {
   render() {
-    return <Layout></Layout>
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/400" component={Error400} />
+          <Route exact path="/401" component={Error401} />
+          <Route exact path="/403" component={Error403} />
+          <Route exact path="/404" component={Error404} />
+          <Route exact path="/500" component={Error500} />
+          <Route exact path="/503" component={Error503} />
+          <Route exact path="/blog" component={BlogPage} />
+          <Route exact path="/email" component={Email} />
+          <Route exact path="/empty-page" component={Empty} />
+          <Route exact path="/form-elements" component={FormElementsPage} />
+          <Route exact path="/forgot-password" component={ForgotPasswordPage} />
+          <Route exact path="/profile" component={ProfilePage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/store" component={StoreCardsPage} />
+          <Route component={Error404} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
---
+++ src/HomePage.react.js
@@ -0,0 +1,99 @@
+// @flow
+
+import * as React from "react";
+
+import {
+  Page,
+  Icon,
+  Grid,
+  Card,
+  Text,
+  Table,
+  Button,
+} from "tabler-react";
+
+import SiteWrapper from "./SiteWrapper.react";
+
+function Home() {
+  return (
+    <SiteWrapper>
+      <Page.Content title="Dashboard">
+        <Grid.Row>
+          <Grid.Col width={12}>
+            <Card title="Invoices">
+              <Table
+                responsive
+                className="card-table table-vcenter text-nowrap"
+                headerItems={[
+                  { content: "No.", className: "w-1" },
+                  { content: "Invoice Subject" },
+                  { content: "Client" },
+                  { content: "VAT No." },
+                  { content: "Created" },
+                  { content: "Status" },
+                  { content: "Price" },
+                  { content: null },
+                  { content: null },
+                ]}
+                bodyItems={[
+                  {
+                    key: "1",
+                    item: [
+                      {
+                        content: (
+                          <Text RootComponent="span" muted>
+                            001401
+                          </Text>
+                        ),
+                      },
+                      {
+                        content: (
+                          <a href="invoice.html" className="text-inherit">
+                            Design Works
+                          </a>
+                        ),
+                      },
+                      { content: "Carlson Limited" },
+                      { content: "87956621" },
+                      { content: "15 Dec 2017" },
+                      {
+                        content: (
+                          <React.Fragment>
+                            <span className="status-icon bg-success" /> Paid
+                          </React.Fragment>
+                        ),
+                      },
+                      { content: "$887" },
+                      {
+                        alignContent: "right",
+                        content: (
+                          <React.Fragment>
+                            <Button size="sm" color="secondary">
+                              Manage
+                            </Button>
+                            <div className="dropdown">
+                              <Button
+                                color="secondary"
+                                size="sm"
+                                isDropdownToggle
+                              >
+                                Actions
+                              </Button>
+                            </div>
+                          </React.Fragment>
+                        ),
+                      },
+                      { content: <Icon link name="edit" /> },
+                    ],
+                  },
+                ]}
+              />
+            </Card>
+          </Grid.Col>
+        </Grid.Row>
+      </Page.Content>
+    </SiteWrapper>
+  );
+}
+
+export default Home;
